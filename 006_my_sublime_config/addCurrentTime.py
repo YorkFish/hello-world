83cf73f5c424ffbb55c79ff69b412b4ba4fcd8f9
York <18258788231@163.com>
2020-09-07 14:38:41 +0800

    add my sublime config

---
+++ 006_my_sublime_config/addCurrentTime.py
@@ -0,0 +1,12 @@
+import datetime
+import sublime
+import sublime_plugin
+
+
+class AddCurrentTimeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        self.view.run_command("insert_snippet", 
+            {
+                "contents": "%s" % datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
+            }
+        )
